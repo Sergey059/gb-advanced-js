2831b21624d5d7add5e5bcf7b08ddc4aa66084cb
Sergey059 <sergeiv-82@mail.ru>
2021-10-11 17:26:25 +0500

    add card item for basket

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductList{
 	constructor(container='.products'){
 		this.container = container;
-		this.goods = [];
-		this.#fetchProducts();
-		this.render();//вывод товаров на страницу
-		this.sumPrice();//подсчет общей цены товаров
+		this.goods = []//массив товаров из JSON документа
+		this.#getProducts()
+			.then(data => { //data - объект js
+				this.goods = data;
+				this.render()
+			});
 	}
-
-	#fetchProducts(){
-		this.goods = [
-			{id: 1, src:'../img/1.jpg', title: 'Notebook', price: 2000},
-			{id: 2, src:'../img/2.jpg', title: 'Mouse', price: 20},
-			{id: 3, src:'../img/3.jpg', title: 'Keyboard', price: 200},
-			{id: 4, src:'../img/4.jpg', title: 'Gamepad', price: 50},
-		];
+	#getProducts(){
+		return fetch(`${API}/catalogData.json`)
+			.then(result => result.json())
+			.catch(error => {console.log(error)})
 	}
-	
 	render(){
 		const block = document.querySelector(this.container);
 		for(let product of this.goods){
@@ -23,62 +22,97 @@ class ProductList{
 			block.insertAdjacentHTML("beforeend",item.render());
 		}
 	}
-
-	// ЗАДАНИЕ 2
-	//--------------------------------------
-	sumPrice(){
-		let sum = 0;
-		for(let product of this.goods){
-			sum+=product.price;
-		}
-		console.log(sum);
-	}
-	
 }
 
 class ProductItem{
-	constructor(product){
-		this.title = product.title;
-		this.id = product.id;
+	constructor(product, img = 'https://via.placeholder.com/200x150'){
+		this.title = product.product_name;
+		this.id = product.id_product;
 		this.price = product.price;
-		this.src = product.src;
+		this.src = img;
 	}
 	render(){
 		return `<div class="product-item" data-id=${this.id}>
 							<img src=${this.src} class="product-img">
 							<div class="product-desc">
 								<h3 class="product-title">${this.title}</h3>
-								<p class="product-price">${this.price} руб.</p>
-								<button class="btn buy-btn">Купить</button>
+								<p class="product-price">${this.price} ₽</p>
+								<button class="btn buy-btn">Добавить в корзину</button>
 							</div>
 						</div>`
 	}
 }
 
-// ЗАДАНИЕ 1
-//--------------------------------------
-class Cart{
 
+class Cart{
+	constructor(container='.cart-list'){
+		this.container = container;
+		this.goodsCart = []
+		this.#getCartProducts()
+			.then(data => {
+				this.goodsCart = data;
+				this.render();
+				this.quantityProductsCart();
+				this.totalPriceCart()
+			});
+	}
+	#getCartProducts(){
+		return fetch(`${API}/getBasket.json`)
+			.then(result => result.json())
+			.catch(error => {console.log(error)})
+	}
+	render(){
+		const blockCart = document.querySelector(this.container);
+		for(let product of this.goodsCart.contents){
+			const itemCart = new CartItem(product);
+			blockCart.insertAdjacentHTML("beforeend", itemCart.render());
+		}
+	}
 	//добавления товара в корзину
 	addProductToCart() {}
-
 	//удаление товара из корзины
 	deleteProductFromCart() {}
-
 	//Количество товаров в корзине
-	quantityProductsCart() {}
-
+	quantityProductsCart() {
+		let productsCartLength = document.querySelector(this.container).children.length;
+		document.querySelector('.quantity').textContent = productsCartLength;
+	}
 	//Общая цена товаров в корзине
-	totalPriceCart() {}
-
+	totalPriceCart() {
+		let totalPrice = document.querySelector('.total'); 
+		let sum = 0;
+		for(let product of this.goodsCart.contents){
+			sum += product.price * product.quantity;
+		}
+		totalPrice.textContent = `${sum} ₽`;
+	}
 }
 
 class CartItem{
-
-	//разметка товара в корзине
-	render(){}
-
+	constructor(product, img = 'https://via.placeholder.com/200x150'){
+		this.title = product.product_name;
+		this.id = product.id_product;
+		this.price = product.price;
+		this.quantity = product.quantity;
+		this.src = img;
+	}
+	render(){
+		return `<li class="cart-item" data-id=${this.id}>
+							<a href="#" class="cart-link">
+								<img src=${this.src} alt="${this.title}" class="cart-img">
+								<div class="cart-desc">
+									<h4 class="cart-title">${this.title}</h4>
+									<div class="cart-price-box">
+										<p class="cart-price">${this.price} ₽</p>
+										<p class="cart-quantity">${this.quantity}</p>
+									</div>
+								</div>
+							</a>
+							<button class="btn delete-btn">&#10006;</button>
+						</li>`
+	}
 }
 
 
 let list = new ProductList();
+let cart = new Cart();
